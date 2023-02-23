02f6f8a40ebb025b0b070b2af8a7075d966085e6
kandymm <kandymm17@gmail.com>
2023-02-23 22:04:48 +0200

    added localstorage

--- counter.js
+++ counter.js
@@ -2,13 +2,14 @@ const counterDom = document.getElementById('counter');
 const increaseDom = document.getElementById('increase');
 const decreaseDom = document.getElementById('decrease');
 let counter = 0;
-counterDom.innerHTML = counter; 
+counterDom.innerHTML = localStorage.getItem('counter') ? Number(localStorage.getItem('counter')) : 0
 
 increaseDom.addEventListener('click',  clickEvent);
 decreaseDom.addEventListener('click', clickEvent);
 
 function clickEvent() {
     this.id === 'increase'? counterDom.innerHTML = counter += 1: counterDom.innerHTML= counter -=1 ;
+    localStorage.setItem('counter', counter  )
 
 };
 
